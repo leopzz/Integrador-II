3e6f6ec529e31a0eea0290a4d441019610829e98
Leo Pellizari <leonardopelizzari77@gmail.com>
2023-11-09 00:32:16 -0300

    Backend

--- Backend/app.js
+++ Backend/app.js
@@ -1,13 +1,40 @@
-const http = require('http');
-const port = 3000;
+const { Sequelize } = require('sequelize');
 
-const server = http.createServer(function (req, res){
-    
-})
+require("dotenv").config();
+const port = process.env.PORTA;
 
-server.listen(port, function(error){
-    if (error)
-        console.log('Deu pau', error);
-    else
-        console.log('Listenting');
-})
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+app.use('/Cadastros', require('./Controllers/Cadastros'));
+
+app.listen(port);
+
+async function connect() {
+    if (global.connection)
+        return global.connection.connect();
+
+    const { Pool } = require('pg');
+    const pool = new Pool({
+        host: process.env.HOST,
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        max: 20,
+        idleTimeoutMillis: 30000,
+        connectionTimeoutMillis: 2000,
+    });
+    const sequelize = new Sequelize(process.env.DB, process.env.USER, process.env.PASSWORD, {
+        host: '',
+        dialect: 'postgres'
+    });
+
+    const client = await pool.connect();
+    client.release();
+    console.log("Servidor iniciado")
+    global.connection = pool;
+    return pool.connect();
+}
+
+
+connect(); 
