d7ed37c34bd71c0b013362efef4aa9007574c679
Leo Pellizari <leonardopelizzari77@gmail.com>
2023-11-15 22:05:17 -0300

    commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Ingrediente from './Views/Ingrediente';
+import Sabor from './Views/Sabor';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path='/Cadastros/Ingrediente' element={<Ingrediente />} />
+        <Route path='/Cadastros/Sabor' element={<Sabor />} />
       </Routes>
     </Router>
   );
---
+++ Frontend/src/Views/Sabor.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Sabor () {
+    return (
+        <>
+            <div>
+                <h1>Pagina sabor</h1>
+            </div>
+        </>
+    );
+};
+
+export default Sabor;
