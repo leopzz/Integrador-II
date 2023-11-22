28add1610b531e3676a53bbcea6434530b7ee95e
Lucas <lucas_glang@hotmail.com>
2023-11-21 23:48:13 -0300

    lindo

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Ingrediente from './Views/Ingrediente';
 import Sabor from './Views/Sabor';
 import Login from './Views/telalogin';
+import Home1 from './Views/home1';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path='/Cadastros/Ingrediente' element={<Ingrediente />} />
         <Route path='/Cadastros/Sabor' element={<Sabor />} />
         <Route path='/telalogin' element={<Login />} />
+        <Route path='/Home1' element={<Home1 />} />
       </Routes>
     </Router>
   );
--- Frontend/src/Views/telalogin.jsx
+++ Frontend/src/Views/telalogin.jsx
@@ -1,63 +1,70 @@
 import React, { useState } from 'react';
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
-import './Login.css';
+import './login.css';
 
 function Login() {
-  const [showChoices, setShowChoices] = useState(false);
+    const [showChoices, setShowChoices] = useState(false);
 
-  const handleLogin = (e) => {
-    e.preventDefault();
-    setShowChoices(!showChoices); // Alterna a visibilidade das escolhas
-  };
+    const handleLogin = (e) => {
+        e.preventDefault();
+        setShowChoices(!showChoices); // Alterna a visibilidade das escolhas
+    };
 
-  const handleRegister = () => {
-    // Implementar redirecionamento para a página de registro
-    console.log('Redirecionar para a página de registro');
-  };
+    const handleRegister = () => {
+        // Implementar redirecionamento para a página de registro
+        console.log('Redirecionar para a página de registro');
+    };
 
-  const handleChoice = (link) => {
-    window.location.href = link; // Redireciona para o link escolhido
-  };
+    const handleChoice = (link) => {
+        window.location.href = link; // Redireciona para o link escolhido
+    };
 
-  return (
-    <>
-      <Container className="d-flex vh-100">
-        <Row className="m-auto align-self-center">
-          <Col md={6} className="mx-auto">
-            <Form>
-              <Form.Group className="mb-3">
-                <Form.Control type="email" placeholder="Digite seu email" />
-              </Form.Group>
-              <Form.Group className="mb-3">
-                <Form.Control type="password" placeholder="Digite sua senha" />
-              </Form.Group>
-              <div className="d-grid gap-2">
-                <Button variant="primary" onClick={handleLogin} size="lg">
-                  Login
-                </Button>
-                <Button variant="secondary" onClick={handleRegister} size="lg">
-                  Registrar
-                </Button>
-              </div>
-            </Form>
-          </Col>
-        </Row>
-        {showChoices && (
-          <Row className="mt-4">
-            <Col md={6} className="mx-auto text-center">
-              <h4>Escolha um Restaurante</h4>
-              <Button variant="primary" onClick={() => handleChoice("http://link1.com")}>
-                Restaurante 1
-              </Button>
-              <Button variant="secondary" onClick={() => handleChoice("http://link2.com")}>
-                Restaurante 2
-              </Button>
-            </Col>
-          </Row>
-        )}
-      </Container>
-    </>
-  );
+    return (
+
+        <>
+            <div className='master'>
+                <div className='login-centro'>
+
+
+                    <Container className="container-login d-flex vh-100">
+                        <Row className="m-auto align-self-center">
+                            <Col md={6} className="mx-auto">
+                                <Form>
+                                    <Form.Group className="mb-3">
+                                        <Form.Control type="email" placeholder="Digite seu email" />
+                                    </Form.Group>
+                                    <Form.Group className="mb-3">
+                                        <Form.Control type="password" placeholder="Digite sua senha" />
+                                    </Form.Group>
+                                    <div className="d-grid gap-2">
+                                        <Button variant="primary" onClick={handleLogin} size="lg">
+                                            Login
+                                        </Button>
+                                        <Button variant="secondary" onClick={handleRegister} size="lg">
+                                            Registrar
+                                        </Button>
+                                    </div>
+                                </Form>
+                            </Col>
+                        </Row>
+                        {showChoices && (
+                            <Row className="mt-4">
+                                <Col md={6} className="mx-auto text-center">
+                                    <h4>Escolha um Restaurante</h4>
+                                    <Button variant="primary" onClick={() => handleChoice("http://localhost:5173/home1")}>
+                                        Restaurante 1
+                                    </Button>
+                                    <Button variant="secondary" onClick={() => handleChoice("http://link2.com")}>
+                                        Restaurante 2
+                                    </Button>
+                                </Col>
+                            </Row>
+                        )}
+                    </Container>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default Login;
